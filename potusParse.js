65e4321b80cd90a3bb9ef1c7a00189d284856263
Everett Yeaw <evyeaw@gmail.com>
2020-10-25 15:15:16 -0400

    parses potus name/bd

---
+++ potusParse.js
@@ -0,0 +1,16 @@
+const rp = require('request-promise');
+const $ = require('cheerio');
+
+const potusParse = function(url) {
+  return rp(url)
+    .then(function(html) {
+      return {
+        name: $('.firstHeading', html).text(),
+        birthday: $('.bday', html).text(),
+      };
+    })
+    .catch(function(err) {
+    });
+};
+
+module.exports = potusParse;
